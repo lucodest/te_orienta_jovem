767aeb20dd1175c8cc96f49387ab7e66acee80b1
lucodest <l_tobias@hotmail.com>
2022-10-24 18:22:03 -0300

    lipezaa 2

--- cadastro.php
+++ cadastro.php
@@ -57,7 +57,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="estilo_cadastro.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo_cadastro.css">
 </head>
 <body>
 <img src="img/background.png" id="bg">
--- login.php
+++ login.php
@@ -63,7 +63,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="estilo_login.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo_login.css">
 </head>
 <body>
 <div class="container">
